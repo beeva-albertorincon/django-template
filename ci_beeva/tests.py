509748920c9a41a0cfe3e2609c308b4f2b81f03f
beeva-albertorincon <alberto.rincon@beeva.com>
2016-01-20 10:00:40 +0100

    sample test

--- ci_beeva/tests.py
+++ ci_beeva/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class ExampleTestCase(TestCase):
+	"""
+	TestCase example
+	"""
+	def setUp(self):
+		"""
+		Initialize test data
+		"""
+		name = 'BEEVA'
+		self.assertEqual(name.lower(), 'beeva')
